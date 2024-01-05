e99d8b246f38100174b07ad85f459482fa75648e
tnomads <59341830+konohadesign@users.noreply.github.com>
2024-01-05 18:13:32 +0100

    new update

--- main.js
+++ main.js
@@ -24,8 +24,39 @@ window.Webflow.push(() => {
       };
     }
   });
-
   console.log("Service Overview Section Attached Event Listener");
+
+  //Selected Works Section Hover Effect
+  document.querySelectorAll(".sw_s-cl-item").forEach((item) => {
+    const imgWrapper = item.querySelector(".sw_preview-img-wrapper");
+    const preview = item.querySelector(".sw_preview");
+
+    // Function to update image position smoothly and center it on the cursor
+    const updateImagePosition = (e) => {
+      const itemRect = item.getBoundingClientRect();
+      // Calculate the center position of the .sw_preview element relative to the cursor
+      const centerX = e.clientX - itemRect.left - preview.offsetWidth / 2;
+      const centerY = e.clientY - itemRect.top - preview.offsetHeight / 2;
+
+      // Use GSAP to smoothly update the position of the .sw_preview element
+      gsap.to(preview, { x: centerX, y: centerY, duration: 0.1 });
+    };
+
+    // Mouse enter event
+    item.addEventListener("mouseenter", (e) => {
+      updateImagePosition(e);
+      gsap.to(imgWrapper, { scale: 1, duration: 0.3, ease: "power1.out" });
+    });
+
+    // Mouse move event
+    item.addEventListener("mousemove", updateImagePosition);
+
+    // Mouse leave event
+    item.addEventListener("mouseleave", () => {
+      gsap.to(imgWrapper, { scale: 0, duration: 0.3, ease: "power1.in" });
+    });
+  });
+
   //Testimonial Section (Infinite Scroll)
   document.addEventListener("DOMContentLoaded", function () {
     const scrollers = document.querySelectorAll(".infinite-scroller");
